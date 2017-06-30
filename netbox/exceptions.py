99db20bfd1faa570fc6462b576624a7f58778b16
Thomas <thomas@tjrb.nl>
2017-06-30 19:35:58 +0200

    added AuthException. Fix style

--- netbox/exceptions.py
+++ netbox/exceptions.py
@@ -1,9 +1,11 @@
 class BaseExceptions(Exception):
     pass
 
+
 class DeleteException(BaseException):
     """Raised when delete failed"""
     pass
+
 
 class NotFoundException(BaseException):
     """Raised when item is not found"""
@@ -14,6 +16,12 @@ class NotFoundException(BaseException):
     def __str__(self):
         return 'Unable to found {}'.format(self.item)
 
+
 class CreateException(BaseException):
     """Raised when creation failed"""
+    pass
+
+
+class AuthException(BaseException):
+    """Raised when an API call method is not allowed"""
     pass
