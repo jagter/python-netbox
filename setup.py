15cc1bdecfb69737ca7a2fff41523ff390980fbf
Thomas v.d. Jagt <thomas.van.der.jagt@columbo.nl>
2017-11-02 08:01:41 +0100

    update version to 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
         return f.read()
 
 setup(name='python-netbox',
-      version='0.0.1',
+      version='0.0.2',
       description='Python NetBox Client',
       long_description=readme(),
       author='Thomas van der Jagt',
