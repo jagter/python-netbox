83c2812839a16e1b08e95ac846d690bb6be37a51
Thomas <thomas@tjrb.nl>
2017-12-12 09:10:49 +0100

    check if key name exist

--- netbox/dcim.py
+++ netbox/dcim.py
@@ -94,14 +94,15 @@ class Dcim(object):
         """Get all devices"""
         return self.netbox_con.get('/dcim/devices/', **kwargs)
 
-    def get_devices_per_rack(self, rack_name, **kwargs):
+    def get_devices_per_rack(self, rack_name, site_name, **kwargs):
         """Get devices which belongs to the given rack
 
         :param rack_name: Name of the rack
+        :param site_name: Name of the site
         :return: list of devices otherwise an empty list
         """
         try:
-            rack_id = self.get_racks(facility_id=rack_name)[0]['id']
+            rack_id = self.get_racks(site=site_name, q=rack_name)[0]['id']
         except IndexError:
             raise exceptions.NotFoundException('rack: {}'.format(rack_name)) from None
         return self.netbox_con.get('/dcim/devices', rack_id=rack_id, **kwargs)
