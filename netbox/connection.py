3591db81e5ee274845c5c91b469c89c118a024d4
Thomas <thomas@tjrb.nl>
2018-08-03 11:03:18 +0200

    test with new session setup

--- netbox/connection.py
+++ netbox/connection.py
@@ -19,24 +19,56 @@ class NetboxConnection(object):
 
         self.base_url = 'http{s}://{host}:{p}{prefix}'.format(s='s' if use_ssl else '', p=self.port, host=self.host,
                                                                prefix='/api' if api_prefix is None else api_prefix)
-        self.session = requests.Session()
-        self.session.verify = ssl_verify
 
-        if auth:
-            self.session.auth = auth
+        self.ssl_verify = ssl_verify
+        self._session = None
 
-        if auth_token:
+        # self.session = requests.Session()
+        # self.session.verify = ssl_verify
+
+        # if auth:
+        #     self.session.auth = auth
+        #
+        # if auth_token:
+        #     token = 'Token {}'.format(self.auth_token)
+        #     self.session.headers.update({'Authorization': token})
+        #     self.session.headers.update({'Accept': 'application/json'})
+        #     self.session.headers.update({'Content-Type': 'application/json'})
+        #
+        # if auth and auth_token:
+        #     raise ValueError('Only one authentication method is possible. Please use auth or auth_token')
+
+    @property
+    def session(self):
+        print('entering session')
+        if not self._session:
+            self._connect()
+        return self._session
+
+    def _connect(self):
+
+        sess = requests.Session()
+        sess.verify = self.ssl_verify
+
+        if self.auth:
+            sess.auth = self.auth
+
+        if self.auth_token:
             token = 'Token {}'.format(self.auth_token)
-            self.session.headers.update({'Authorization': token})
-            self.session.headers.update({'Accept': 'application/json'})
-            self.session.headers.update({'Content-Type': 'application/json'})
+            sess.headers.update({'Authorization': token})
+            sess.headers.update({'Accept': 'application/json'})
+            sess.headers.update({'Content-Type': 'application/json'})
 
-        if auth and auth_token:
+        if self.auth and self.auth_token:
             raise ValueError('Only one authentication method is possible. Please use auth or auth_token')
+
+        print('entering _connect')
+        print(sess)
+        self._session = sess
 
     def __request(self, method, params=None, key=None, body=None, url=None):
 
-        if method is 'post' or 'patch' or 'delete':
+        if method != 'GET':
             if not self.auth_token:
                 raise exceptions.AuthException('Authentication credentials were not provided')
 
@@ -50,16 +82,16 @@ class NetboxConnection(object):
                 url = self.base_url + str(params)
 
         request = requests.Request(method=method, url=url, json=body)
+        print(self._session)
         prepared_request = self.session.prepare_request(request)
+        print(prepared_request)
 
         try:
             response = self.session.send(prepared_request)
         except socket.gaierror:
-            err_msg = 'Unable to find address: {}'.format(self.host)
-            raise socket.gaierror(err_msg)
+            raise socket.gaierror('Unable to find address: {}'.format(self.host))
         except requests.exceptions.ConnectionError:
-            err_msg = 'Unable to connect to Netbox host: {}'.format(self.host)
-            raise ConnectionError(err_msg)
+            raise ConnectionError('Unable to connect to Netbox host: {}'.format(self.host))
         except requests.exceptions.Timeout:
             raise TimeoutError('Connection to Netbox host timed out')
         except Exception as e:
@@ -75,7 +107,6 @@ class NetboxConnection(object):
         return response.ok, response.status_code, response_data
 
     def get(self, param, key=None, **kwargs):
-
         if kwargs:
             url = '{}{}?{}'.format(self.base_url, param,
                                    '&'.join('{}={}'.format(key, val) for key, val in kwargs.items()))
