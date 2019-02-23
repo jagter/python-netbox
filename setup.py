4a22a7e333dea9ab176a9b81c256788b2b84a23b
Thomas <thomas@tjrb.nl>
2019-02-23 20:25:45 +0100

    Prepared setup for version 0.13

--- setup.py
+++ setup.py
@@ -3,22 +3,25 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setup(name='python-netbox',
-      version='0.0.12',
+      version='0.0.13',
       description='Python NetBox Client',
       long_description=readme(),
       python_requires='>=3',
       author='Thomas van der Jagt',
       author_email='thomas@tjrb.nl',
       url='https://github.com/jagter/python-netbox',
-      download_url='https://github.com/jagter/python-netbox/releases/tag/0.0.12.tar.gz',
+      download_url='https://github.com/jagter/python-netbox/releases/tag/0.0.13.tar.gz',
       packages=find_packages(),
       install_requires=['ipaddress', 'requests'],
       classifiers = [
